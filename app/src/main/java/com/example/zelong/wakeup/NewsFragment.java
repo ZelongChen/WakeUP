52e8bb45485f637ac7ae9f3298016aef44756f40
ZelongCHEN <chen.zelong0814@gmail.com>
2016-02-28 16:08:45 +0100

    add hastag want to use to search and show tweets

--- app/src/main/java/com/example/zelong/wakeup/NewsFragment.java
+++ app/src/main/java/com/example/zelong/wakeup/NewsFragment.java
@@ -1,18 +1,36 @@
 package com.example.zelong.wakeup;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.zelong.wakeup.Tools.HashTagPreference;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
+import java.util.Set;
 
 
-public class NewsFragment extends ListFragment {
+public class NewsFragment extends Fragment {
+
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private String[] tags;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -22,7 +40,6 @@ public class NewsFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(false);
-        showTweets();
     }
 
     @Override
@@ -30,19 +47,53 @@ public class NewsFragment extends ListFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_news, container, false);
+        listView = (ListView) view.findViewById(R.id.hashtag_listview);
+        updateTags();
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tags);
+        listView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInputDialog();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String key = tags[position];
+                Intent intent = new Intent(getActivity(), TweetsActivity.class);
+                intent.putExtra("SEARCH_KEY", key);
+                startActivity(intent);
+            }
+        });
+
 
         return view;
     }
 
-    private void showTweets() {
-        SearchTimeline timeline = new SearchTimeline.Builder()
-                .query("#Football")
-                .query("#NBA")
-                .build();
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
-                .setTimeline(timeline)
-                .build();
-        setListAdapter(adapter);
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Change city");
+        final EditText input = new EditText(getActivity());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                new HashTagPreference(getActivity()).addHashTag(input.getText().toString());
+                updateTags();
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.show();
     }
-    
+
+    private void updateTags() {
+        Set<String> hashtags = new HashTagPreference(getActivity()).getHashTags();
+        tags = hashtags.toArray(new String[hashtags.size()]);
+    }
+
 }
---
+++ app/src/main/java/com/example/zelong/wakeup/Tools/HashTagPreference.java
@@ -0,0 +1,38 @@
+package com.example.zelong.wakeup.Tools;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by zelong on 2/28/16.
+ */
+public class HashTagPreference {
+
+    SharedPreferences prefs;
+    private Activity activity;
+    Set<String> defaultHashTags = new HashSet<>();
+    public HashTagPreference(Activity activity){
+        this.activity = activity;
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+        defaultHashTags.add(new CityPreference(activity).getCity());
+    }
+
+    // If the user has not chosen a city yet, return
+    // Sydney as the default city
+    public Set<String> getHashTags(){
+        return prefs.getStringSet("hashTags", defaultHashTags);
+    }
+
+    public void setHashTags(Set<String> hashTags){
+        prefs.edit().putStringSet("hashTags", hashTags).commit();
+    }
+
+    public void addHashTag(String hashtag) {
+        Set<String> hashtags = getHashTags();
+        hashtags.add(hashtag);
+        setHashTags(hashtags);
+    }
+}
