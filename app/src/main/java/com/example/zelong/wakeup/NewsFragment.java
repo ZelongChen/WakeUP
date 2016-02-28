f2f7f8ed50b9f6e59f7a51a1952a33127b503feb
ZelongCHEN <chen.zelong0814@gmail.com>
2016-02-28 14:44:37 +0100

    show search tweets

--- app/src/main/java/com/example/zelong/wakeup/NewsFragment.java
+++ app/src/main/java/com/example/zelong/wakeup/NewsFragment.java
@@ -1,33 +1,18 @@
 package com.example.zelong.wakeup;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.example.zelong.wakeup.Tools.RestClient;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthToken;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
 
-public class NewsFragment extends Fragment {
 
-    final static String CONSUMER_KEY = "JUwUAjWimjkSQGUHbcf8hKMvz";
-    final static String CONSUMER_SECRET = "PSO4jVUCbCCnXVktRFKBjUzSEuMZW8QGmfMha8yKKwgcvHp4lw";
-    final static String TwitterTokenURL = "https://api.twitter.com/oauth2/token";
-    final static String TwitterStreamURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
-
-    private TwitterLoginButton loginButton;
+public class NewsFragment extends ListFragment {
 
     public NewsFragment() {
         // Required empty public constructor
@@ -37,6 +22,7 @@ public class NewsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(false);
+        showTweets();
     }
 
     @Override
@@ -45,36 +31,18 @@ public class NewsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_news, container, false);
 
-        loginButton = (TwitterLoginButton) view.findViewById(R.id.login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-
-                TwitterSession session = Twitter.getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                String token = authToken.token;
-                String secret = authToken.secret;
-
-                Toast.makeText(getActivity(), "login success", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
-                Toast.makeText(getActivity(), "login fail", Toast.LENGTH_SHORT).show();
-            }
-        });
-
         return view;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result to the login button.
-        loginButton.onActivityResult(requestCode, resultCode, data);
+    private void showTweets() {
+        SearchTimeline timeline = new SearchTimeline.Builder()
+                .query("#Football")
+                .query("#NBA")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
+                .setTimeline(timeline)
+                .build();
+        setListAdapter(adapter);
     }
     
 }
