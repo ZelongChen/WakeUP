e00ccf67d8fe7f1c93023815f7bdab941facae8d
ZelongCHEN <chen.zelong0814@gmail.com>
2016-03-03 11:00:05 +0100

    add check listener

--- app/src/main/java/com/example/zelong/wakeup/ControlFragment.java
+++ app/src/main/java/com/example/zelong/wakeup/ControlFragment.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.zelong.wakeup.Tools.RestClient;
 
 
 public class ControlFragment extends Fragment {
@@ -21,11 +25,47 @@ public class ControlFragment extends Fragment {
         setHasOptionsMenu(false);
     }
 
+    private Switch lightSwitch;
+    private Switch tvSwitch;
+    private Switch speakerSwitch;
+    private Switch musicSwitch;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_control, container, false);
+        lightSwitch = (Switch) view.findViewById(R.id.light_switch);
+        tvSwitch = (Switch) view.findViewById(R.id.tv_switch);
+        speakerSwitch = (Switch) view.findViewById(R.id.speaker_switch);
+        musicSwitch = (Switch) view.findViewById(R.id.music_switch);
+
+        lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+
+        tvSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        speakerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
 
         return  view;
     }
